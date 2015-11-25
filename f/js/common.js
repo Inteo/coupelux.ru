42ded6daf0db883863d0028eec79675abd72526d
BerTepesh <Ber.Tepesh@gmail.com>
2015-11-25 19:41:41 +0400

    +

--- f/js/common.js
+++ f/js/common.js
@@ -1,6 +1,23 @@
 
 $(document).ready(function(){
-	
+	/*var scrTop = $('html, body').scrollTop();
+  $(".sec_2 .sec__back").each(function(){
+    //var elTop = $(this).offset().top;
+    //console.log(elTop + ' - ' + scrTop);
+    $(this).height(scrTop);
+  });*/
+});
+
+$(document).scroll(function(){
+  
+  //$(".sec_1 .sec__back").offset({top: -$(this).scrollTop(), left: 0});
+  //$(".sec_2 .sec__back").offset({top: -$(this).scrollTop(), left: 0});
+  /*var scrTop = $(this).scrollTop();
+  $(".sec__back").each(function(){
+    var elTop = $(this).offset().top;
+    console.log(elTop + ' - ' + scrTop);
+    //$(this).offset({top: -scrTop + top   , left: 0});
+  });*/
 });
 $(function(){
 	
