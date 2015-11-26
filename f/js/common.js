6bcc44cbd2ca621d68f9f71a0a2bb38fd891cf63
BerTepesh <Ber.Tepesh@gmail.com>
2015-11-26 18:53:11 +0400

    +

--- f/js/common.js
+++ f/js/common.js
@@ -1,24 +1,26 @@
 
 $(document).ready(function(){
-	/*var scrTop = $('html, body').scrollTop();
-  $(".sec_2 .sec__back").each(function(){
-    //var elTop = $(this).offset().top;
-    //console.log(elTop + ' - ' + scrTop);
-    $(this).height(scrTop);
-  });*/
+
 });
 
 $(document).scroll(function(){
   
-  //$(".sec_1 .sec__back").offset({top: -$(this).scrollTop(), left: 0});
-  //$(".sec_2 .sec__back").offset({top: -$(this).scrollTop(), left: 0});
-  /*var scrTop = $(this).scrollTop();
-  $(".sec__back").each(function(){
-    var elTop = $(this).offset().top;
-    console.log(elTop + ' - ' + scrTop);
-    //$(this).offset({top: -scrTop + top   , left: 0});
-  });*/
 });
+
 $(function(){
-	
+	$(".modal-ham").fancybox({
+    type: 'inline',
+    wrapCSS: 'ham-popup',
+    fixed: true,
+    title: '',
+    padding: 0,
+    autoResize: false,
+    autoCenter: false,
+    fitToView: false,
+    helpers: {
+      overlay: {
+        //fixed: false
+      }
+    }
+  });
 });
